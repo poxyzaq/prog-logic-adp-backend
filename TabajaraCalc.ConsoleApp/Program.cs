fc642e02ee1fa3102930730a93438dca6b1b05cd
vnyenv <panyra@proton.me>
2026-03-15 23:39:45 -0300

    First commit

---
+++ TabajaraCalc.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace TabajaraCalc.ConsoleApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    etteec
+    tetet
+        Console.WriteLine("Hello, World!");
+        Console.WriteC
+    }
+}
